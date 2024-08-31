f90361825c71c70ed88aa88906dd8b5430afe89c
Premchand <premchandmali001@gmail.com>
2024-08-31 11:54:35 +0530

    the change of search

--- src/api/models/search.js
+++ src/api/models/search.js
@@ -5,13 +5,30 @@ exports.get_search_product = async (query) => {
     code = 500,
     data = [];
   try {
-    const search = await db.query(`SELECT * FROM product WHERE product_name LIKE ?`, [`%${query.query}%`]);
-    (message = "No product found"), (code = 404), (data = []);
-    if (search.length) {
-      (message = "Product fetched successfully"), (code = 200), (data = search);
+    // Query to search in the product table
+    const productSearch = await db.query(
+      `SELECT * FROM product WHERE product_name LIKE ?`,
+      [`%${query.query}%`]
+    );
+
+    // Query to search in the pooja table
+    const poojaSearch = await db.query(
+      `SELECT * FROM pooja WHERE pooja_name LIKE ?`,
+      [`%${query.query}%`]
+    );
+
+    // Combine the results
+    data = [...productSearch, ...poojaSearch];
+
+    if (data.length) {
+      message = "Search results fetched successfully";
+      code = 200;
+    } else {
+      message = "No results found";
+      code = 404;
     }
   } catch (error) {
-    message = error;
+    message = error.message || "An error occurred";
   }
-  return {message,code,data};
+  return { message, code, data };
 };
